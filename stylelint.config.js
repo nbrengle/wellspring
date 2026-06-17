3250d7076463318795e2e2c41de1d1d4bfe0aa25
Nate Mooney <nmooney@Nates-MacBook-Pro.local>
2026-06-17 12:16:10 -0700

    Fix CSS stylelint errors and enable strict checks

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,23 +2,6 @@ export default {
   extends: ["stylelint-config-standard"],
   rules: {
     "selector-class-pattern": null,
-    "color-function-notation": null,
-    "color-function-alias-notation": null,
-    "alpha-value-notation": null,
-    "rule-empty-line-before": null,
-    "no-descending-specificity": null,
-    "declaration-block-single-line-max-declarations": null,
-    "font-family-name-quotes": null,
-    "declaration-block-no-shorthand-property-overrides": null,
-    "value-keyword-case": null,
-    "media-feature-range-notation": null,
-    "at-rule-empty-line-before": null,
-    "comment-empty-line-before": null,
-    "declaration-block-no-redundant-longhand-properties": null,
-    "no-duplicate-selectors": null,
-    "property-no-vendor-prefix": null,
-    "declaration-property-value-keyword-no-deprecated": null,
-    "shorthand-property-no-redundant-values": null,
-    "color-hex-length": null
+    "color-function-notation": null
   },
 };
